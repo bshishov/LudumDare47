f0b70ef1c70f4460f172b1a0c766ef224cf26dc2
DESKTOP-ANE7Q38\User <arfr@live.ru>
2020-10-04 00:54:29 +0200

    Sloppy following the player UI

---
+++ UnityGame/Assets/Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Camera;
+
+    private Vector3 _dif;
+
+    void Start()
+    {
+        _dif = transform.parent.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = transform.position - Camera.transform.position;
+        v.x = 0;
+        v.z = 0;
+        transform.rotation = Quaternion.LookRotation(v - Camera.transform.position);
+        transform.position = transform.parent.position - _dif;
+    }
+}
