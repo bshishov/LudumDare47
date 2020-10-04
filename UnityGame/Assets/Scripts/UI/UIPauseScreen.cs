0e0f3c81b06a7ffcdb2824cfecee84c302ea0d91
DESKTOP-ANE7Q38\User <arfr@live.ru>
2020-10-05 00:28:41 +0200

    Fader added

---
+++ UnityGame/Assets/Scripts/UI/UIPauseScreen.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utils.UI
+{
+    [RequireComponent(typeof(UICanvasGroupFader))]
+    public class UIPauseScreen : MonoBehaviour
+    {
+        private UICanvasGroupFader _fader;
+
+        void Start()
+        {
+            _fader = GetComponent<UICanvasGroupFader>();
+            _fader.StateChanged += StateChanged;
+        }
+
+        private void StateChanged()
+        {
+            if (_fader.State == UICanvasGroupFader.FaderState.FadedIn)
+            {
+                Time.timeScale = 0f;
+            }
+
+            if (_fader.State == UICanvasGroupFader.FaderState.FadedOut)
+            {
+                Time.timeScale = 1f;
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (_fader.State == UICanvasGroupFader.FaderState.FadedIn)
+                    _fader.FadeOut();
+
+                if (_fader.State == UICanvasGroupFader.FaderState.FadedOut)
+                    _fader.FadeIn();
+            }
+        }
+    }
+}
