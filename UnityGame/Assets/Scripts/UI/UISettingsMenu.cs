20df572602f5826d4f245fc072932160a51d651c
DESKTOP-ANE7Q38\User <arfr@live.ru>
2020-10-04 22:42:18 +0200

    Settings and menu

---
+++ UnityGame/Assets/Scripts/UI/UISettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class UISettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+
+}
