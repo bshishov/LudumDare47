6ec243f787e4a896fba2b99bd35aed52de46874a
DESKTOP-ANE7Q38\User <arfr@live.ru>
2020-10-04 21:13:42 +0200

    UI

---
+++ UnityGame/Assets/Scripts/UI/UIChooseLevel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class UIChooseLevel : MonoBehaviour
+    {
+        public Text TextNumber;
+        private bool _unlocked = false;
+        private string _levelName;
+        
+
+        private void Start()
+        {
+           
+        }
+
+        public void SetSceneSettings(int number, string levelName)
+        {
+            _levelName = levelName;
+            TextNumber.text = number.ToString();
+            if (PlayerPrefs.GetInt(string.Format(levelName), 0) == 1)
+            {
+                _unlocked = true;
+                // VISUALS
+            }
+            else
+            {
+                // VISUALS
+            }
+
+        }
+
+        public void LoadLevel()
+        {
+            if (_unlocked)
+                SceneManager.LoadScene(_levelName);
+        }
+    }
+}
---
+++ UnityGame/Assets/Scripts/UI/UISceneSelector.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISceneSelector : MonoBehaviour
+{
+    public GameObject ParentObject;
+    public GameObject LevelPrefab;
+    public Object[] scenes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            var panel = Instantiate(LevelPrefab);
+            panel.transform.SetParent(ParentObject.transform, false);
+            panel.GetComponent<UIChooseLevel>().SetSceneSettings(i+1, scenes[i].name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
