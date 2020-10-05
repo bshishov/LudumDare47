fa7a51a3bc858a6a39e60c8229d2076090b65d60
DESKTOP-ANE7Q38\User <arfr@live.ru>
2020-10-05 14:25:24 +0200

    Back to menu

--- UnityGame/Assets/Scripts/UI/UILoad.cs
+++ UnityGame/Assets/Scripts/UI/UILoad.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UILoad : MonoBehaviour
 {
+    public string MainMenu;
     public UICanvasGroupFader _fader;
 
     void Start()
     {
         _fader.FadeOut();
+    }
+
+    public void LoadMenu()
+    {
+        _fader.FadeIn();
+        _fader.StateChanged += () =>
+        {
+            if (_fader.State == UICanvasGroupFader.FaderState.FadedIn)
+            {
+                SceneManager.LoadScene(MainMenu); 
+            }
+        };
+        
     }
 
 }
