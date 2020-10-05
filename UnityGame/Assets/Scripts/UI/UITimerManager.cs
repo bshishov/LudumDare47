86508cdfb1389206659cc1828ecef6a9eaf600ce
DESKTOP-ANE7Q38\User <arfr@live.ru>
2020-10-05 19:03:42 +0200

    TimerUi

---
+++ UnityGame/Assets/Scripts/UI/UITimerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimerManager : MonoBehaviour
+{
+    public GameObject WorldCanvas;
+    private Dictionary<int, GameObject> _objectToCanvas = new Dictionary<int, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void SetTimer(GameObject parent, int timer)
+    {
+        var panel = (GameObject)Instantiate(WorldCanvas);
+        panel.transform.SetParent(parent.transform, false);
+        panel.GetComponentInChildren<Text>().text = timer.ToString();
+        _objectToCanvas[parent.GetInstanceID()] = panel;
+    }
+    public void DeleteTimer(GameObject parent)
+    {        
+        GameObject.Destroy(_objectToCanvas[parent.GetInstanceID()]);
+    }
+}
