3bdca658d58877a8c489c0d8cffb694898115bae
Roman_HOME <noagard@gmail.com>
2021-05-29 17:39:52 +0300

    Create SwipeDetector script

---
+++ UnityGame/Assets/Scripts/Gameplay/SwipeDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetector: MonoBehaviour
+{
+
+    [SerializeField]
+    private float minDistanceForSwipe = 10f;
+
+    private Vector2 fingerUpPosition;
+    private Vector2 fingerDownPosition;
+
+
+    private void Update()
+    {
+
+
+        
+    }
+}
