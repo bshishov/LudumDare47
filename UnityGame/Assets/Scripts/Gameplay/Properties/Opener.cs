acba616e1b56e5570bbcfe9cd3f2d53386ecab2d
Roman_HOME <noagard@gmail.com>
2021-06-02 11:56:27 +0300

    Change all materials shader on mobile

--- UnityGame/Assets/Scripts/Gameplay/Properties/Opener.cs
+++ UnityGame/Assets/Scripts/Gameplay/Properties/Opener.cs
@@ -6,39 +6,39 @@ namespace Gameplay.Properties
 {
 
     [RequireComponent(typeof(Entity))]
-    public class Opener : MonoBehaviour//, ICommandHandler
+    public class Opener : MonoBehaviour, ICommandHandler
     {
-        /*
+        
         [SerializeField]
         private List<GameObject> _openableObjects;
 
         public void OnInitialized(Level level)
         {
-            throw new System.NotImplementedException();
+            
         }
 
-        public IEnumerable<IChange> Handle(Level level, ICommand command)
-        {
-            throw new System.NotImplementedException();
-        }
+        
 
         public void OnAfterPlayerMove(Level level)
         {
-            throw new System.NotImplementedException();
+            
         }
 
         
 
         public void OnTurnRolledBack(Level level)
         {
-            throw new System.NotImplementedException();
+           
         }
 
         public void Revert(Level level, IChange change)
         {
+          
+        }
+
+        public IEnumerable<IChange> Handle(Level level, ICommand command)
+        {
             throw new System.NotImplementedException();
         }
-        */
-      
     }
 }
