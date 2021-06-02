1e907beaa52c83655398bab092aa061998c451ff
Roman_HOME <noagard@gmail.com>
2021-06-02 17:15:43 +0300

    Started implementing collectable mechanic

---
+++ UnityGame/Assets/Scripts/Gameplay/Properties/Collectable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay.Properties
+{
+    public class Collectable : MonoBehaviour, ICommandHandler
+    {
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        public IEnumerable<IChange> Handle(Level level, ICommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnAfterPlayerMove(Level level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnInitialized(Level level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnTurnRolledBack(Level level)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Revert(Level level, IChange change)
+        {
+            throw new System.NotImplementedException();
+        }
+
+       
+    }
+}
