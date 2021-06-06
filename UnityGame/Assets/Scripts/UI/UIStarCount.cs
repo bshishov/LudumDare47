c485e06fcd575fcb568bded67ac0094da085246f
Roman <noagard@gmail.com>
2021-06-06 14:35:56 +0300

    add UI implementation for stars

---
+++ UnityGame/Assets/Scripts/UI/UIStarCount.cs
@@ -0,0 +1,22 @@
+﻿using Gameplay;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIStarCount : MonoBehaviour
+    {
+        private Text _numberOfStarText;
+
+        private void Start()
+        {
+            _numberOfStarText = GetComponent<Text>();
+        }
+
+
+        private void Update()
+        {
+            _numberOfStarText.text = "Stars on level - " + GameSettings.NumberOfStarsOnLevel + "\n" + "Total starts - " + GameSettings.TotalNumberOfStars;
+        }
+    }
+}
