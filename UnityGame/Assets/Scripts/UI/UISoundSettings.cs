b88f91c11a4815c70fc718c8a37346d8e4b2453e
Roman_HOME <noagard@gmail.com>
2021-06-14 14:40:41 +0300

    add sound Icon script

--- UnityGame/Assets/Scripts/UI/UISoundSettings.cs
+++ UnityGame/Assets/Scripts/UI/UISoundSettings.cs
@@ -1,12 +1,40 @@
 ﻿using UnityEngine;
+using Gameplay;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class UISoundSettings : MonoBehaviour
     {
+        public Image SoundOn;
+        public Image SoundOff;
+
+        private Button _soundButton;
         private void Start()
         {
+            _soundButton = GetComponent<Button>();
+            _soundButton.onClick.AddListener(ChangeSound);
 
+            SoundIconChange();
+        }
+
+        private void SoundIconChange()
+        {
+            if (PlayerStats.Instance.SoundStatus)
+            {
+                SoundOn.enabled = true;
+                SoundOff.enabled = false;
+            } else
+            {
+                SoundOn.enabled = false;
+                SoundOff.enabled = true;
+            }
+        }
+
+        private void ChangeSound()
+        {
+            PlayerStats.Instance.SoundStatus = !PlayerStats.Instance.SoundStatus;
+            SoundIconChange();
         }
     }
 }
