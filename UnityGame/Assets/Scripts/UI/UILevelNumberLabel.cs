6410242574e48e966fa028ba5b836b4d3a735afb
Boris Shishov <borisshishov@gmail.com>
2021-08-22 14:29:24 +0300

    LevelNumberLabel

---
+++ UnityGame/Assets/Scripts/UI/UILevelNumberLabel.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class UILevelNumberLabel : MonoBehaviour
+    {
+        public TextMeshProUGUI Label;
+
+        void Start()
+        {
+            // This is a dirty hack to get level number
+            var sceneName = SceneManager.GetActiveScene().name;
+            var numberString = Regex.Match(sceneName, @"\d+").Value;
+            if (!string.IsNullOrEmpty(numberString))
+            {
+                Label.text = numberString;
+            }
+        }
+    }
+}
